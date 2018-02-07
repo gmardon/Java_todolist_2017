1b134c2bd17544e44a054a2b67f0541c0adce52d
Guillaume MARDON <contact@gmardon.com>
2018-02-07 15:21:29 +0100

    useless cleaning

--- app/src/main/java/com/gmardon/todolist/TaskListActivity.java
+++ app/src/main/java/com/gmardon/todolist/TaskListActivity.java
@@ -1,23 +1,15 @@
 package com.gmardon.todolist;
 
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
-import android.os.Parcelable;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.os.Parcelable;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
